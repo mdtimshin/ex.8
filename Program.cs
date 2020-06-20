29c667faa9a60fa69b354fa50dbaadf0482e4d2d
mdtimshin <63156611+mdtimshin@users.noreply.github.com>
2020-06-20 22:28:12 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq.Expressions;
 
 namespace ex._8
 {
@@ -8,44 +9,70 @@ namespace ex._8
     {
         static void Main(string[] args)
         {
-            string[] lines = File.ReadAllLines("graf_matrix.txt");
-            int[,] matrix = new int[lines.Length, lines.Length];
-            for(int i=0; i<lines.Length; i++)
+            try
             {
-                string[] s = lines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                for(int k=0; k<lines.Length; k++)
+                string[] lines = File.ReadAllLines("graf_matrix.txt");
+                for(int i = 0; i<lines.Length; i++)
                 {
-                    matrix[i, k] = int.Parse(s[k]);
-                }
-            }
-            List<string> blocks = new List<string>();
-            List<int> tochkiSochl = new List<int>();
-            for(int i=0; i<lines.Length; i++)
-            {
-                for(int j=0; j<lines.Length; j++)
-                {
-                    if ((matrix[i, j] == 1)&&(!tochkiSochl.Contains(j)))
+                    if (lines[0].Length != lines[i].Length)
                     {
-                        tochkiSochl.Add(j);
+                        Console.WriteLine("Матрица не квадратная. Завершение работы программы");
+                        Environment.Exit(1);
                     }
                 }
-                for(int j=0; j<tochkiSochl.Count-1; j++)
+                int[,] matrix = new int[lines.Length, lines.Length];
+                if (lines.Length == 0)
                 {
-                    for(int k=j+1; k<tochkiSochl.Count; k++)
+                    Console.WriteLine("Файл graf_matrix.txt пуст. Завершение работы программы");
+                    Environment.Exit(1);
+                }
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string[] s = lines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    for (int k = 0; k < lines.Length; k++)
                     {
-                        if (matrix[j, k] == 0)
+                        matrix[i, k] = int.Parse(s[k]);
+                    }
+                }
+                List<string> blocks = new List<string>();
+                List<int> tochkiSochl = new List<int>();
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    for (int j = 0; j < lines.Length; j++)
+                    {
+                        if ((matrix[i, j] == 1) && (!tochkiSochl.Contains(j)))
                         {
-                            blocks.Add("V" + i + "V" + k);
+                            tochkiSochl.Add(j);
+                        }
+                    }
+                    for (int j = 0; j < tochkiSochl.Count - 1; j++)
+                    {
+                        for (int k = j + 1; k < tochkiSochl.Count; k++)
+                        {
+                            if (matrix[j, k] == 0 && i != k)
+                            {
+                                if (!blocks.Contains("V" + i + "V" + k) && !blocks.Contains("V" + k + "V" + i))
+                                    blocks.Add("V" + i + "V" + k);
+                            }
                         }
                     }
                 }
-                //blocks.Clear();
+                Console.WriteLine("Блоки в графе:");
+                foreach (string s in blocks)
+                {
+                    Console.WriteLine(s);
+                }
             }
-            foreach(string s in blocks)
+            catch (System.FormatException)
             {
-                Console.WriteLine(s);
+                Console.WriteLine("В матрице присутствуют литеры");
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("Файл graf_matrix.txt не найден. Завершение работы программы");
             }
             Console.ReadLine();
+            
         }
     }
 }
